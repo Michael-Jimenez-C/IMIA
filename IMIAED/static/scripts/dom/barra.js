55f80869735ec00f49409499e5c8d829a8641517
Michael-Steven-Jimenez-contreras <72152481+Michael-Steven-Jimenez-contreras@users.noreply.github.com>
2022-02-25 16:10:58 -0500

    +

--- IMIAED/static/scripts/dom/barra.js
+++
@@ -1,7 +0,0 @@
-import "../api/docprop.js";
-class Barra{
-    constructor(document){
-        body=document.getElementById("root");
-        body.innerHTML+="<Header><img src='"+ruta+"recursos/logo.jpeg'><Header/>"
-    }
-}
