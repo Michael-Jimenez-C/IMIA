ee4cff86ecf4f1642d427616d14bb09f82b31813
Michael-Steven-Jimenez-contreras <72152481+Michael-Steven-Jimenez-contreras@users.noreply.github.com>
2022-02-25 15:51:05 -0500

    -

---
+++ IMIAED/static/scripts/dom/barra.js
@@ -0,0 +1,7 @@
+import "../api/docprop.js";
+class Barra{
+    constructor(document){
+        body=document.getElementById("root");
+        body.innerHTML+="<Header><img src='"+ruta+"recursos/logo.jpeg'><Header/>"
+    }
+}
--- IMIAED/static/scripts/dom/main.js
+++ IMIAED/static/scripts/dom/main.js
@@ -1,5 +1,9 @@
 const lorem="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 function load(){
     body=document.getElementById("root");
+    head=document.getElementById("head");
+    head+='<script type="text/javascript" src="../static/scripts/dom/barra.js"></script>'
+    head+='<script type="text/javascript" src="../static/scripts/api/docprop.js"></script>'
+    barra=Barra(document);
     body.innerHTML+=lorem;
 }
