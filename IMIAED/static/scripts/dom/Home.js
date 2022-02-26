821dc2cb0ec3586660b77eb441b3b8704a5d2967
Michael-Steven-Jimenez-contreras <72152481+Michael-Steven-Jimenez-contreras@users.noreply.github.com>
2022-02-26 17:55:25 -0500

    Mejore la interfaz y agregue un menu

---
+++ IMIAED/static/scripts/dom/Home.js
@@ -0,0 +1,11 @@
+import Recursos from './Recursos.js'
+class Home{
+    constructor(){
+        Recursos.menu();
+    }
+    actualizar(){
+        /*setTimeout('slider()',5000);*/
+    }
+
+    }
+new Home();
--- IMIAED/static/scripts/dom/main.js
+++
@@ -1,9 +0,0 @@
-function load(){
-    /*barra(document);
-    document.getElementById("root").innerHTML+=f();*/
-}
-
-function barra(doc){/*
-    body =doc.getElementById("root");
-    body.innerHTML=("<header><img src='../static/recursos/Logo.svg'></header>")+body.innerHTML;*/
-}
