2251ec244fa0dff504fff32d3e1fe6dd8af295d6
Michael-Steven-Jimenez-contreras <72152481+Michael-Steven-Jimenez-contreras@users.noreply.github.com>
2022-02-25 22:59:22 -0500

    Cree los botones

--- IMIAED/static/scripts/dom/main.js
+++ IMIAED/static/scripts/dom/main.js
@@ -4,5 +4,5 @@ function load(){
 
 function barra(doc){
     body =doc.getElementById("root");
-    body.innerHTML=("<header><img src='../static/recursos/logo.jpeg'><H1>IMIA Editor</H1></header>")+body.innerHTML;
+    body.innerHTML=("<header><img src='../static/recursos/Logo.svg'></header>")+body.innerHTML;
 }
