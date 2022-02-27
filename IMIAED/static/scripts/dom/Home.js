daf0552b1f07d9c6d045b67c16beedf5f49a5e2a
Michael-Steven-Jimenez-contreras <72152481+Michael-Steven-Jimenez-contreras@users.noreply.github.com>
2022-02-26 22:54:54 -0500

    el menu ahora se puede construir desde javascript

--- IMIAED/static/scripts/dom/Home.js
+++ IMIAED/static/scripts/dom/Home.js
@@ -1,21 +1,13 @@
 import Recursos from './Recursos.js'
 class Home{
     constructor(){
-        Recursos.menu();
-        this.subMenus=document.querySelectorAll(".desplegable");
-        this.subMenus.forEach(i=>{
-            i.addEventListener('click',()=>{
-                i.classList.toggle('desplegado');
-                let menu=i.nextElementSibling;
-                menu.classList.toggle('oculto');
-                menu.classList.toggle('mostrando');
-                
-            });
-        });
+        Recursos.menu();  
     }
     actualizar(){
         /*setTimeout('slider()',5000);*/
     }
 
-    }
+}
+
+
 new Home();
--- IMIAED/static/scripts/dom/Recursos.js
+++ IMIAED/static/scripts/dom/Recursos.js
@@ -1,4 +1,18 @@
 export default class Recursos{
     static menu(){
+        let body=document.getElementById("root");
+        body.innerHTML="<header><img src='../static/recursos/Logo.svg'><nav><ul class='menu'> <li><div class='desplegable'><a>Prueba 1</a><img src='../static/recursos/flecha.svg'></div><ul class='subMenu oculto'><li><a>Prueba 1</a></li><li><a>Prueba 2</a></li><li><a>Prueba 3</a></li></ul></li><li><a>Prueba 2</a></li><li><a>Prueba 3</a></li></ul></nav></header>"+body.innerHTML;
+        
+        
+        this.subMenus=document.querySelectorAll(".desplegable");
+        this.subMenus.forEach(i=>{
+            i.addEventListener('click',()=>{
+                i.classList.toggle('desplegado');
+                let menu=i.nextElementSibling;
+                menu.classList.toggle('oculto');
+                menu.classList.toggle('mostrando');
+                
+            });
+        });
     }
 }
