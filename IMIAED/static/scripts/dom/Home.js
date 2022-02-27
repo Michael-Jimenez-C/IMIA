f2235898a1282eccead0914e7ddd58f5cc75cd0a
Michael-Steven-Jimenez-contreras <72152481+Michael-Steven-Jimenez-contreras@users.noreply.github.com>
2022-02-26 22:18:46 -0500

    mejora del menu

--- IMIAED/static/scripts/dom/Home.js
+++ IMIAED/static/scripts/dom/Home.js
@@ -2,6 +2,16 @@ import Recursos from './Recursos.js'
 class Home{
     constructor(){
         Recursos.menu();
+        this.subMenus=document.querySelectorAll(".desplegable");
+        this.subMenus.forEach(i=>{
+            i.addEventListener('click',()=>{
+                i.classList.toggle('desplegado');
+                let menu=i.nextElementSibling;
+                menu.classList.toggle('oculto');
+                menu.classList.toggle('mostrando');
+                
+            });
+        });
     }
     actualizar(){
         /*setTimeout('slider()',5000);*/
