df4e3976e3ad2bf550c82c69ee884be1345356d2
Michael-Steven-Jimenez-contreras <72152481+Michael-Steven-Jimenez-contreras@users.noreply.github.com>
2022-03-20 18:16:54 -0500

    -

--- IMIAED/Servidor.py
+++ IMIAED/Servidor.py
@@ -9,4 +9,12 @@ def Home():
 def Indice(id):
     return render_template(id)
 
+@app.route("/filter/<filtro>/<img1>")
+def filtro(flitro, img1):
+    return None
+
+@app.route("/filter/<filtro>/<img1>/<img2>")
+def filtroP2(flitro, img1, img2):
+    return None
+
 app.run()
